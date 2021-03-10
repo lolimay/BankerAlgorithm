1402146cef673add7380e69d1d3cbd0764059d60
lolimay <lolimay@lolimay.cn>
2021-03-10 14:43:34 +0800

    Introduce react into project

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+function App() {
+    return (
+        <div>Hello World!</div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
